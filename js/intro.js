53ead3e670bc902d30cf424b041cd24a32dfad9b
Zonneveld <pim.zonneveld@capgemini.com>
2020-03-06 16:43:07 +0100

    Presentation:

--- js/intro.js
+++ js/intro.js
@@ -2,7 +2,7 @@ var audio = new Audio('../assets/chopsuey.mp3');
 audio.play();
 
 var start = function() {
-    window.location.href = '../pages/zombies.html'
+    window.location.href = '../pages/zombies.html';
 }
 
 setTimeout(function(){ 
