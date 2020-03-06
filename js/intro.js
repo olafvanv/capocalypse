094dfd74f96bc0ad907cd22c2725a2e21eb91e86
Olaf van Venetie <olafvv@Olafs-MacBook-Pro.local>
2020-03-06 14:20:34 +0100

    link to zombies

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,10 @@
 var audio = new Audio('../assets/chopsuey.mp3');
 audio.play();
 
+var start = function() {
+    window.location.href = '../pages/zombies.html'
+}
+
 setTimeout(function(){ 
     document.querySelector('.intro-button').className = 'intro-button visible';
 }, 40000);
